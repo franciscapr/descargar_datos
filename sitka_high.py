d874534f7a99527e601172a2789662a7e3d7a711
Francisca <francisca.paredes.pr@gmail.com>
2025-03-17 12:17:04 -0300

    trazado

--- sitka_high.py
+++ sitka_high.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import csv
 
+import matplotlib.pyplot as plt
+
 
 path = Path('weather_data/sitka_weather_07-2021_simple.csv')
 lines = path.read_text().splitlines()
@@ -20,3 +22,15 @@ for row in reader:
     highs.append(high)
 
 print(highs)
+
+# Traza las temperatuas màximas.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, color='red')
+
+# Da formato al trazado.
+ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+ax.set_ylabel('Temperatura (F)', fontsize=16)
+ax.tick_params(labelsize=16)
+plt.show()
