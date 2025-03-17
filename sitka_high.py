79b89cd69aefee0968b02397fd354cbbcd5d03e1
Francisca <francisca.paredes.pr@gmail.com>
2025-03-17 12:29:46 -0300

    obtencion de las fechas

--- sitka_high.py
+++ sitka_high.py
@@ -3,6 +3,8 @@ import csv
 
 import matplotlib.pyplot as plt
 
+from datetime import datetime
+
 
 path = Path('weather_data/sitka_weather_07-2021_simple.csv')
 lines = path.read_text().splitlines()
@@ -10,27 +12,25 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
+# Obtiene fechas y temperaturas màximas de este archivo.
+dates, highs = [], []
 
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
-
-# Extraer las temperaturas màximas.
-
-highs = []
 for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    dates.append(current_date)
     highs.append(high)
 
-print(highs)
-
-# Traza las temperatuas màximas.
+# Traza las temperaturas màximas.
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(highs, color='red')
+ax.plot(dates, highs, color="red")
+
 
 # Da formato al trazado.
 ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()    # Dibuja las etiquetas de las fechas diagonalmente
 ax.set_ylabel('Temperatura (F)', fontsize=16)
 ax.tick_params(labelsize=16)
 plt.show()
